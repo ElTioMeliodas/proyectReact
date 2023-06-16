8de6a286dbadc096331c58c717e9109f4fa6535f
Mauricio Rivas <mauricio.rivas@vasslatam.com>
2023-06-16 02:04:21 -0400

    new project

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Parrafo from './components/Parrafo'
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo!!</h1>
+      <Parrafo />
+    </div>
+  );
+}
+
+export default App;
